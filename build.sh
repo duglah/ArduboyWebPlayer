0895a5f9536c7761ff64979a0652c3c20795b5f9
Philipp Manstein <philipp.gm@googlemail.com>
2019-09-01 17:33:32 +0200

    Add debug flag, remove gen files moving, refactored paths

--- build.sh
+++ build.sh
@@ -3,6 +3,4 @@ mkdir build
 cd build
 #cmake ../cpp
 #make
-emcc ../cpp/helloworld.cpp ../cpp/Arduboy.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop]" -s TOTAL_MEMORY=16MB --js-library ../js/library.js -o helloworld.js
-mv helloworld.js ../web/gen/
-mv helloworld.wasm ../web/gen/
+emcc -g4 ../cpp/helloworld.cpp ../cpp/Arduboy.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop]" -s TOTAL_MEMORY=16MB --js-library ../js/library.js -o helloworld.js --source-map-base http://localhost:8080/../build/
