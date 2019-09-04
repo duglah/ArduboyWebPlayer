023904439133a1c9eb59559dafc03b9912f3a622
Philipp Manstein <philipp.gm@googlemail.com>
2019-09-04 23:40:32 +0200

    Add rendering to buffer and add non block delay

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir build
 cd build
 #cmake ../cpp
 #make
-emcc -g4 ../cpp/helloworld.cpp ../cpp/Arduboy.cpp ../cpp/Arduino.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop]" -s TOTAL_MEMORY=16MB --js-library ../js/library.js -o helloworld.js --source-map-base http://localhost:8080/../build/
+emcc -g4 ../cpp/helloworld.cpp ../cpp/Arduboy.cpp ../cpp/Arduino.cpp ../cpp/core.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop, _main]" -s TOTAL_MEMORY=16MB -s ASYNCIFY=1 --js-library ../js/library.js -o helloworld.js --source-map-base http://localhost:8080/../build/
