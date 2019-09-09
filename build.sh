8ae3ce4d25c13b2cef1a77cf92326e01a9cb1b2a
Philipp Manstein <philipp.gm@googlemail.com>
2019-09-09 14:45:23 +0200

    Add more code

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ mkdir build
 cd build
 #cmake ../cpp
 #make
-emcc -g4 ../cpp/helloworld.cpp ../cpp/Arduboy.cpp ../cpp/Arduino.cpp ../cpp/core.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop, _main]" -s TOTAL_MEMORY=16MB -s ASYNCIFY=1 --js-library ../js/library.js -o helloworld.js --source-map-base http://localhost:8080/../build/
+emcc -g4 ../cpp/helloworld.cpp ../cpp/Arduboy.cpp ../cpp/Arduino.cpp ../cpp/core/core.cpp ../cpp/Print.cpp ../cpp/audio/audio.cpp -s WASM=1 -s EXPORTED_FUNCTIONS="[_setup, _loop, _main]" -s TOTAL_MEMORY=16MB -s ASYNCIFY=1 --js-library ../js/library.js -o helloworld.js --source-map-base http://localhost:8080/../build/
